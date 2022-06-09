e26696d7bcd0e19f62a1ba632f84d43ee254b256
great-jin <great__jin@163.com>
2022-06-09 17:45:11 +0800

    规范整理

--- spring-boot/src/main/java/com/budai/thread/ThreadTest.java
+++ spring-boot/src/main/java/com/budai/thread/ThreadTest.java
@@ -1,13 +1,11 @@
 package com.budai.thread;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 public class ThreadTest {
 
     @Autowired
-    @Qualifier(value = "taskExecutor")
     private ThreadPoolTaskExecutor taskExecutor;
 
 }
